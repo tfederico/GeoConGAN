9c814207be60a910eaceac7499c010f12a9e228b
tfederico <federicotavella.7@gmail.com>
2019-07-11 12:38:13 +0100

    Rewrite training loop

--- geocongan.py
+++ geocongan.py
@@ -22,7 +22,7 @@ from cyclegan import Discriminator, CycleGenerator
 from silnet import SilNet
 from utils_cyclegan import imshow, scale, print_models, view_samples
 from utils_silnet import reverse_transform, masks_to_colorimg
-from loss import silnet_loss#, cycle_consistency_loss
+from loss import silnet_loss, cycle_consistency_loss
 from hands_dataset import HandsDataset
 
 def weights_init_normal(m):
@@ -43,11 +43,11 @@ class LambdaLR():
     def step(self, epoch):
         return 1.0 - max(0, epoch + self.offset - self.decay_start_epoch)/((self.n_epochs - self.decay_start_epoch) + 1)
 
-def get_data_loader(image_type, tfs, image_dir='synth2real', 
+def get_data_loader(image_type, tfs, image_dir='synth2real',
                     image_size=256, batch_size=8, num_workers=0):
-    """Returns training and test data loaders for a given image type, either 'synth' or 'real'. 
+    """Returns training and test data loaders for a given image type, either 'synth' or 'real'.
     """
-    
+
     # get training and test directories
     image_path = './' + image_dir
     train_path = os.path.join(image_path, image_type)
@@ -65,7 +65,7 @@ def get_data_loader(image_type, tfs, image_dir='synth2real',
 
 def create_model(n_res_blocks=9, device="cpu"):
     """Builds the generators and discriminators."""
-    
+
     # Instantiate generators
     G_XtoY = CycleGenerator(n_residual_blocks=n_res_blocks)
     G_YtoX = CycleGenerator(n_residual_blocks=n_res_blocks)
@@ -87,12 +87,12 @@ def create_model(n_res_blocks=9, device="cpu"):
 
 
 def training_loop(dataloader_X, dataloader_Y, test_dataloader_X, test_dataloader_Y, n_epochs=200):
-    
+
     print_every = 1#n_epochs//10
-    
+
     # keep track of losses over time
     losses = []
-    
+
     test_iter_X = iter(test_dataloader_X)
     test_iter_Y = iter(test_dataloader_Y)
 
@@ -100,19 +100,19 @@ def training_loop(dataloader_X, dataloader_Y, test_dataloader_X, test_dataloader
     # constant throughout training, that allow us to inspect the model's performance.
     fixed_X = test_iter_X.next()[0]
     fixed_Y = test_iter_Y.next()[0]
-    
+
 
     for epoch in range(1, n_epochs+1):
 
         for batch_X, batch_Y in zip(dataloader_X, dataloader_Y):
-        
+
 
             images_X, silhouette_X = batch_X
             #images_X = scale(images_X) # make sure to scale to a range -1 to 1
 
             images_Y, silhouette_Y = batch_Y
             #images_Y = scale(images_Y)
-             
+
             # move images to GPU if available (otherwise stay on CPU)
             device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
             images_X = images_X.to(device)
@@ -120,133 +120,77 @@ def training_loop(dataloader_X, dataloader_Y, test_dataloader_X, test_dataloader
             silhouette_X = silhouette_X.to(device)
             silhouette_Y = silhouette_Y.to(device)
 
-            # =========================================
-            #            TRAIN THE GENERATORS
-            # =========================================
 
-            ##    First: generate fake X images and reconstructed Y images    ##
+            # FORWARD PASS
+
+            fake_X = G_YtoX(images_Y)
+            fake_Y = G_XtoY(images_X)
+            reconstructed_Y = G_XtoY(fake_X)
+            reconstructed_X = G_YtoX(fake_Y)
+
+            sil_fake_X = torch.sigmoid(S(fake_X))
+            sil_fake_Y = torch.sigmoid(S(fake_Y))
+
+            # SET REQUIRES GRADIENT FALSE ???
+            D_X.eval()
+            D_Y.eval()
+
             g_optimizer.zero_grad()
 
-            # 0. Identity loss
-            #same_X = G_YtoX(images_X)
-            #id_YtoX_loss = criterion_identity(same_X, images_X)*5.0
+            out_x = D_X(fake_Y)
+            out_y = D_Y(fake_X)
 
-            # 1. Generate fake images that look like domain X based on real images in domain Y
-            fake_X = G_YtoX(images_Y)
-            
-
-            # 2. Compute the generator loss based on domain X
-            out_x = D_X(fake_X)
-            
             g_YtoX_loss = real_mse_loss(out_x, real.expand_as(out_x))
-
-            # 3. Create a reconstructed y
-            # 4. Compute the cycle consistency loss (the reconstruction loss)
-            reconstructed_Y = G_XtoY(fake_X)
-            reconstructed_y_loss = cycle_consistency_loss(images_Y, reconstructed_Y, lambda_weight=10)
-
-
-            ##    Second: generate fake Y images and reconstructed X images    ##
-
-            # 0. Identity loss
-            #same_Y = G_XtoY(images_Y)
-            #id_XtoY_loss = criterion_identity(same_Y, images_Y)*5.0
-
-            # 1. Generate fake images that look like domain Y based on real images in domain X
-            fake_Y = G_XtoY(images_X)
-
-            # 2. Compute the generator loss based on domain Y
-            out_y = D_Y(fake_Y)
+            reconstructed_Y_loss = cycle_consistency_loss(reconstructed_Y, images_Y, lambda_weight=10)
             g_XtoY_loss = real_mse_loss(out_y, real.expand_as(out_y))
+            reconstructed_X_loss = cycle_consistency_loss(reconstructed_X, images_X, lambda_weight=10)
+            geo_loss_X = silnet_loss(fake_X, images_X)
+            geo_loss_Y = silnet_loss(fake_Y, images_Y)
 
-            # 3. Create a reconstructed x
-            # 4. Compute the cycle consistency loss (the reconstruction loss)
-            reconstructed_X = G_YtoX(fake_Y)
-            reconstructed_x_loss = cycle_consistency_loss(images_X, reconstructed_X, lambda_weight=10)
+            g_loss = g_YtoX_loss + g_XtoY_loss + reconstructed_X_loss + reconstructed_Y_loss + geo_loss_X + geo_loss_Y
 
-            # 5. Add up all generator and reconstructed losses and perform backprop
-            g_total_loss = g_YtoX_loss + g_XtoY_loss + reconstructed_y_loss + reconstructed_x_loss #+ id_YtoX_loss + id_XtoY_loss
-            g_total_loss.backward(retain_graph=True)
+            g_loss.backward()
             g_optimizer.step()
 
-            # ============================================
-            #            TRAIN THE DISCRIMINATORS
-            # ============================================
 
-            ##   First: D_X, real and fake loss components   ##
+            # SET REQUIRES GRADIENT TRUE ???
 
-            # Train with real images
-            #d_x_optimizer.zero_grad()
+            D_X.train()
+            D_Y.train()
+
             d_optimizer.zero_grad()
 
-            # 1. Compute the discriminator losses on real images
-            out_x = D_X(images_X)
-            D_X_real_loss = real_mse_loss(out_x, real.expand_as(out_x))
-            
-            # Train with fake images
-            
-            # 2. Generate fake images that look like domain X based on real images in domain Y
-            fake_X = G_YtoX(images_Y)
+            out_x_real = D_X(real_Y)
+            d_loss_X_real = real_mse_loss(out_x_real, real.expand_as(out_x_real))
+            out_x_fake = D_X(fake_Y)
+            d_loss_X_fake = fake_mse_loss(out_x_fake, fake.expand_as(out_x_fake))
 
-            # 3. Compute the fake loss for D_X
-            out_x = D_X(fake_X)
-            D_X_fake_loss = fake_mse_loss(out_x, fake.expand_as(out_x))
-            
+            d_X_loss = (d_loss_X_real + d_loss_X_fake) * 0.5
 
-            # 4. Compute the total loss and perform backprop
-            d_x_loss = (D_X_real_loss + D_X_fake_loss)*0.5
-            d_x_loss.backward()
-            #d_x_optimizer.step()
+            d_X_loss.backward()
 
-            
-            ##   Second: D_Y, real and fake loss components   ##
-            
-            # Train with real images
-            #d_y_optimizer.zero_grad()
-            
-            # 1. Compute the discriminator losses on real images
-            out_y = D_Y(images_Y)
-            D_Y_real_loss = real_mse_loss(out_y, real.expand_as(out_y))
-            
-            # Train with fake images
+            out_y_real = D_Y(real_X)
+            d_loss_Y_real = real_mse_loss(out_y_real, real.expand_as(out_y_real))
+            out_y_fake = D_Y(fake_X)
+            d_loss_Y_fake = fake_mse_loss(out_y_fake, fake.expand_as(out_y_fake))
 
-            # 2. Generate fake images that look like domain Y based on real images in domain X
-            fake_Y = G_XtoY(images_X)
+            d_Y_loss = (d_loss_Y_real + d_loss_Y_fake) * 0.5
 
-            # 3. Compute the fake loss for D_Y
-            out_y = D_Y(fake_Y)
-            D_Y_fake_loss = fake_mse_loss(out_y, fake.expand_as(out_y))
+            d_Y_loss.backward()
 
-            # 4. Compute the total loss and perform backprop
-            d_y_loss = (D_Y_real_loss + D_Y_fake_loss)*0.5
-            d_y_loss.backward()
-            #d_y_optimizer.step()
             d_optimizer.step()
 
-            # =========================================
-            #            TRAIN SILNET
-            # =========================================
 
-            sil_optimizer.zero_grad()
 
-            silhouette_reconstructed_X = torch.sigmoid(S(reconstructed_X))
-            silhouette_reconstructed_Y = torch.sigmoid(S(reconstructed_Y))
-
-            sil_loss_X = silnet_loss(silhouette_reconstructed_X, silhouette_X)
-            sil_loss_Y = silnet_loss(silhouette_reconstructed_Y, silhouette_Y)
-            sil_loss = sil_loss_X + sil_loss_Y
-            sil_loss.backward()
-            sil_optimizer.step()
-        
 
         # Print the log info
         if epoch % print_every == 0:
             # append real and fake discriminator losses and the generator loss
-            losses.append((d_x_loss.item(), d_y_loss.item(), g_total_loss.item(), sil_loss.item()))
-            print('Epoch [{:5d}/{:5d}] | d_X_loss: {:6.4f} | d_Y_loss: {:6.4f} | g_total_loss: {:6.4f} | sil_loss: {:6.4f}'.format(
-                    epoch, n_epochs, d_x_loss.item(), d_y_loss.item(), g_total_loss.item(), sil_loss.item()))
+            losses.append((d_X_loss.item(), d_Y_loss.item(), g_loss.item()))
+            print('Epoch [{:5d}/{:5d}] | d_X_loss: {:6.4f} | d_Y_loss: {:6.4f} | g_total_loss: {:6.4f}'.format(
+                    epoch, n_epochs, d_x_loss.item(), d_y_loss.item(), g_total_loss.item()))
 
-            
+
         sample_every = n_epochs/10
         # Save the generated samples
         if epoch % sample_every == 0:
@@ -259,8 +203,7 @@ def training_loop(dataloader_X, dataloader_Y, test_dataloader_X, test_dataloader
         # Update learning rates
         g_lr_scheduler.step()
         d_lr_scheduler.step()
-        s_lr_scheduler.step()
-        
+
         checkpoint(epoch, G_XtoY, G_YtoX, D_X, D_Y, S)
 
     return losses
@@ -289,8 +232,8 @@ gamma = 0.2
 
 n_epochs = 200
 
-features_train_transforms = transforms.Compose([ #transforms.Resize(int(image_size*1.11), Image.BICUBIC), 
-                #transforms.RandomCrop(image_size), 
+features_train_transforms = transforms.Compose([ #transforms.Resize(int(image_size*1.11), Image.BICUBIC),
+                #transforms.RandomCrop(image_size),
                 #transforms.RandomHorizontalFlip(),
                 transforms.ToTensor(),
                 transforms.Normalize((0.5,0.5,0.5), (0.5,0.5,0.5)) ])
@@ -300,8 +243,8 @@ mask_train_transforms = transforms.Compose([
 
 train_transforms = {'feature': features_train_transforms, 'target': mask_train_transforms}
 
-"""train_transforms = transforms.Compose([ #transforms.Resize(int(image_size*1.11), Image.BICUBIC), 
-                #transforms.RandomCrop(image_size), 
+"""train_transforms = transforms.Compose([ #transforms.Resize(int(image_size*1.11), Image.BICUBIC),
+                #transforms.RandomCrop(image_size),
                 #transforms.RandomHorizontalFlip(),
                 transforms.ToTensor(),
                 transforms.Normalize((0.5,0.5,0.5), (0.5,0.5,0.5)) ])"""
@@ -342,17 +285,17 @@ D_Y.apply(weights_init_normal)
 
 
 
-g_params = list(G_XtoY.parameters()) + list(G_YtoX.parameters())  # Get generator parameters
+g_params = list(G_XtoY.parameters()) + list(G_YtoX.parameters()) + list(S.parameters()) # Get generator parameters
 d_params = list(D_X.parameters()) + list(D_Y.parameters())
 
 g_optimizer = optim.Adam(g_params, lr, [beta1, beta2])
 d_optimizer = optim.Adam(d_params, lr, [beta1, beta2])
-sil_optimizer = optim.Adam(S.parameters(), lr, [beta1, beta2])
+#sil_optimizer = optim.Adam(S.parameters(), lr, [beta1, beta2])
 
 # Create learning rate schedulers for generators and discriminators
 g_lr_scheduler = torch.optim.lr_scheduler.LambdaLR(g_optimizer, lr_lambda=LambdaLR(n_epochs, 1, 100).step)
 d_lr_scheduler = torch.optim.lr_scheduler.LambdaLR(d_optimizer, lr_lambda=LambdaLR(n_epochs, 1, 100).step)
-s_lr_scheduler = torch.optim.lr_scheduler.LambdaLR(d_optimizer, lr_lambda=LambdaLR(n_epochs, 1, 100).step)
+#s_lr_scheduler = torch.optim.lr_scheduler.LambdaLR(d_optimizer, lr_lambda=LambdaLR(n_epochs, 1, 100).step)
 
 # Lossess (SilNet and cycle-consistency losses are imported)
 criterion_identity = torch.nn.L1Loss()
@@ -381,4 +324,3 @@ view_samples(n_epochs//10, 'samples_geocongan')
 
 # view samples at iteration 100
 view_samples(n_epochs, 'samples_geocongan')
-
