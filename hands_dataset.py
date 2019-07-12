98c20a37128e40c685672405c387ea7f59273dfa
tfederico <federicotavella.7@gmail.com>
2019-07-12 17:06:22 +0100

    Change file extention

--- hands_dataset.py
+++ hands_dataset.py
@@ -9,7 +9,7 @@ from os.path import isfile, join
 
 class HandsDataset(Dataset):
 	def __init__(self, image_type, path, transform=None):
-		
+
 		self.image_type = image_type
 		self.path = path
 		onlyfiles = [f for f in listdir(path) if isfile(join(path, f))]
@@ -23,17 +23,17 @@ class HandsDataset(Dataset):
 		return self.size
 
 	def __getitem__(self, idx):
-		
+
 		idx = str(int(idx)) # why??
-		images_ext = ".png" if self.image_type == "synth" else ".jpg"
-		
+		images_ext = ".png"
+
 		feature_img_name = os.path.join(self.path, self.feat_prefix + idx + images_ext)
 		target_img_name = os.path.join(self.path, self.targ_prefix + idx + images_ext)
 		feature_image = Image.open(feature_img_name)
 		target_image = Image.open(target_img_name)
 		feature_image = np.array(feature_image)
 		target_image = np.array(target_image)
-		
+
 		if self.feat_transform:
 			feature_image = self.feat_transform(feature_image)
 		if self.targ_transform:
